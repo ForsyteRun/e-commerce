647995fc970c4e760387c658479958c1c9c3469c
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-10 16:24:27 +0300

    refactor: corrections to error message at RoutingError

--- src/pages/RoutingError/components/RoutingErrorMessage/index.tsx
+++ src/pages/RoutingError/components/RoutingErrorMessage/index.tsx
@@ -2,14 +2,20 @@ import { isRouteErrorResponse } from 'react-router-dom';
 import styles from './RoutingErrorMessage.module.scss';
 
 function RoutingErrorMessage(error: unknown): JSX.Element {
+  let message = 'Something went wrong...';
+
   if (isRouteErrorResponse(error)) {
     const { status, statusText } = error;
-    return (
-      <h2 className={styles.errorMessage}>{`${status && `${status} - `}
-      ${statusText && statusText.toUpperCase()}`}</h2>
-    );
+    const is404 = status === 404;
+
+    message = `
+      ${status && `${status} - `}
+      ${is404 && 'PAGE '}
+      ${statusText && statusText}
+    `;
   }
-  return <h2>Something went wrong...</h2>;
+
+  return <h2 className={styles.errorMessage}>{message.toUpperCase()}</h2>;
 }
 
 export default RoutingErrorMessage;
