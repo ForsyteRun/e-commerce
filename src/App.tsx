cb084da4bfebfbb0a6199246c300f3bd7e065c02
ForsyteRun <forsyterun@gmail.com>
2023-08-10 12:34:55 +0300

    feat: add lastName, firstName, password & email

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Registration from './modules/Registration';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Shop</h1>
+      <Registration />
+    </>
+  );
 }
 
 export default App;
---
+++ src/modules/Registration/components/registration.tsx
@@ -0,0 +1,146 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Formik, Form, Field } from 'formik';
+import s from './registration.module.scss';
+
+function validateEmail(value: string) {
+  let error;
+  if (!value) {
+    error = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    error = 'Invalid firstName address';
+  }
+  return error;
+}
+
+function validateFirstName(value: string) {
+  let error;
+  if (!value) {
+    error = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    error = 'Invalid firstName address';
+  }
+  return error;
+}
+
+function validateLastName(value: string) {
+  let error;
+  if (!value) {
+    error = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    error = 'Invalid firstName address';
+  }
+  return error;
+}
+
+function validatePassword(value: string) {
+  let error;
+  if (!value) {
+    error = 'Required';
+  }
+  return error;
+}
+
+function Registration() {
+  return (
+    <div className={s.register}>
+      <h2 className={s.title}>I’m new here</h2>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ errors, touched }) => (
+          <Form method="post" action="register" className={s.form}>
+            <div className={s.input__container}>
+              <label htmlFor="firstName" className={s.label}>
+                FirstName
+              </label>
+              <Field
+                name="firstName"
+                validate={validateFirstName}
+                className={s.input}
+                placeholder="First Name*"
+              />
+              {errors.firstName && touched.firstName && (
+                <div className={s.errorValid}>{errors.firstName}</div>
+              )}
+            </div>
+            <div className={s.input__container}>
+              <label htmlFor="lastName">LastName</label>
+              <Field
+                name="lastName"
+                validate={validateLastName}
+                placeholder="Last Name*"
+                className={s.input}
+              />
+              {errors.lastName && touched.lastName && (
+                <div className={s.errorValid}>{errors.lastName}</div>
+              )}
+            </div>
+            <div className={s.input__container}>
+              <label htmlFor="email">Email</label>
+              <Field
+                name="email"
+                validate={validateEmail}
+                placeholder="email*"
+                className={s.input}
+              />
+              {errors.email && touched.email && (
+                <div className={s.errorValid}>{errors.email}</div>
+              )}
+            </div>
+            <div className={s.input__container}>
+              <label htmlFor="password">Password</label>
+              <Field
+                type="password"
+                name="password"
+                validate={validatePassword}
+                placeholder="password*"
+                className={s.input}
+              />
+              {errors.password && touched.password && (
+                <div className={s.errorValid}>{errors.password}</div>
+              )}
+            </div>
+            <div className={s.select__container}>
+              <Field
+                as="select"
+                name="date"
+                onChange={(e: string) => console.log(e)}
+                className={s.select}
+              >
+                <option value="">1</option>
+                <option value="">2</option>
+              </Field>
+              <Field
+                as="select"
+                name="month"
+                onChange={(e: string) => console.log(e)}
+                className={s.select}
+              >
+                <option value="">1</option>
+                <option value="">2</option>
+              </Field>
+              <Field
+                as="select"
+                name="year"
+                onChange={(e: string) => console.log(e)}
+                className={s.select}
+              >
+                <option value="">1</option>
+                <option value="">2</option>
+              </Field>
+            </div>
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
+
+export default Registration;
