28a1bd1cbb718c8f5e152bd44910ef37c86f20c0
ForsyteRun <forsyterun@gmail.com>
2023-08-11 20:29:51 +0300

    feat: correct select implement

---
+++ src/modules/Registration/components/registration/types.ts
@@ -0,0 +1,9 @@
+export interface InitialValue {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  date: string;
+  month: string;
+  year: string;
+}
--- src/modules/Registration/constants/index.ts
+++ src/modules/Registration/constants/index.ts
@@ -2,3 +2,18 @@ export const START_DAYS = 1;
 export const END_DAYS = 31;
 export const START_YEAR = 1905;
 export const END_YEAR = 2023;
+
+export const allMonths = [
+  'Январь',
+  'Февраль',
+  'Март',
+  'Апрель',
+  'Май',
+  'Июнь',
+  'Июль',
+  'Август',
+  'Сентябрь',
+  'Октябрь',
+  'Ноябрь',
+  'Декабрь',
+];
