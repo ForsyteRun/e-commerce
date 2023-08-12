3090d8d22884969665819fbea7611e85d3d7f8a4
Andrei Paleshka <andrepoleshko@gmail.com>
2023-08-12 22:01:59 +0200

    feat: implement email field

---
+++ src/pages/LoginPage/components/LoginForm/components/EmailField/index.tsx
@@ -0,0 +1,31 @@
+import { Field } from 'formik';
+import { LoginEmailFieldProps } from '../../../../../../types';
+import validateEmail from '../../helpers/validateEmail';
+import styles from './EmailField.module.scss';
+
+function EmailField({
+  values,
+  handleChange,
+  handleBlur,
+}: LoginEmailFieldProps): JSX.Element {
+  return (
+    <div className={styles.inputContainer}>
+      <label htmlFor="email">
+        <span>Email</span>
+      </label>
+      <Field
+        type="email"
+        name="email"
+        id="email"
+        value={values.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder="user@example.com"
+        className={styles.inputEmail}
+        validate={validateEmail}
+      />
+    </div>
+  );
+}
+
+export default EmailField;
