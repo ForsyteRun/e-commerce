c66dc2e085a7c53aa71d229fcd861356cf6f801f
ForsyteRun <forsyterun@gmail.com>
2023-08-13 10:00:29 +0300

    refactor: validation to separate files

--- src/modules/Registration/components/registration/validation/index.ts
+++ src/modules/Registration/components/registration/validation/index.ts
@@ -1,46 +1,5 @@
-const validateEmail = (value: string): string | undefined => {
-  let error;
-  if (!value) {
-    error = 'Required';
-  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
-    error = 'Invalid Email';
-  }
-  return error;
-};
-
-const validateName = (name: string): string | undefined => {
-  let error;
-  if (name.trim() === '') {
-    error = 'Required';
-  } else if (!/^[A-Za-z]+$/.test(name)) {
-    error = 'Must contain only letters';
-  }
-
-  return error;
-};
-
-const validatePassword = (password: string): string | undefined => {
-  let error;
-  if (password.length < 8) {
-    error = 'Min length 8 items';
-  }
-
-  const hasNumber = /\d/.test(password);
-  if (!hasNumber) {
-    error = 'You need at least one number';
-  }
-
-  const hasUppercase = /[A-Z]/.test(password);
-  if (!hasUppercase) {
-    error = 'You need at least one uppercase letter';
-  }
-
-  const hasLowercase = /[a-z]/.test(password);
-  if (!hasLowercase) {
-    error = 'You need at least one lowercase letter';
-  }
-
-  return error;
-};
+import validateEmail from './validateEmail';
+import validateName from './validateName';
+import validatePassword from './validatePassword';
 
 export { validateName, validateEmail, validatePassword };
