455d2fb76a359bfacb77ad1d316977b9a28ae3be
Andrei Paleshka <andrepoleshko@gmail.com>
2023-08-14 08:21:47 +0200

    fix: change password validation test

--- src/pages/LoginPage/components/LoginForm/helpers/validatePassword.test.tsx
+++ src/pages/LoginPage/components/LoginForm/helpers/validatePassword.test.tsx
@@ -6,56 +6,22 @@ describe('Validate password', () => {
     expect(errorMessage).toBe('This field is required');
   });
 
-  it('should return an error message for a password with less than 8 characters', () => {
-    const errorMessage = validatePassword('short');
-    expect(errorMessage).toBe(
-      'Password must contain at least 8 characters long'
-    );
-  });
+  it('should return an error message for invalid password', () => {
+    const invalidPasswords = [
+      'short',
+      'passwordWithoutDigit!',
+      'passwordwithoutuppercase1!',
+      'PASSWORDWITHOUTLOWERCASE1!',
+      'PasswordWithoutSpecial1',
+      ' PasswordWithSpace1!',
+      'PasswordWithSpace1! ',
+    ];
 
-  it('should return an error message for a password without a digit', () => {
-    const errorMessage = validatePassword('PasswordWithoutDigit');
-    expect(errorMessage).toBe('Password must contain at least one digit');
-  });
+    const errorMessage =
+      'Password must be at least 8 characters long and contain at least 1 lowercase, 1 uppercase letters, 1 digit and 1 special character, also, not contain leading or trailing whitespace.';
 
-  it('should return an error message for a password without an uppercase letter', () => {
-    const errorMessage = validatePassword('passwordwithoutuppercase1');
-    expect(errorMessage).toBe(
-      'Password must contain at least one uppercase letter'
-    );
-  });
-
-  it('should return an error message for a password without a lowercase letter', () => {
-    const errorMessage = validatePassword('PASSWORDWITHOUTLOWERCASE1');
-    expect(errorMessage).toBe(
-      'Password must contain at least one lowercase letter'
-    );
-  });
-
-  it('should return an error message for a password without a special character', () => {
-    const errorMessage = validatePassword('PasswordWithoutSpecial1');
-    expect(errorMessage).toBe(
-      'Password must contain at least one special character'
-    );
-  });
-
-  it('should return an error message for a password with leading whitespace', () => {
-    const errorMessage = validatePassword('  PasswordWithSpace1!');
-    expect(errorMessage).toBe(
-      'Password must not contain leading or trailing whitespace'
-    );
-  });
-
-  it('should return an error message for a password with trailing whitespace', () => {
-    const errorMessage = validatePassword('PasswordWithSpace1!  ');
-    expect(errorMessage).toBe(
-      'Password must not contain leading or trailing whitespace'
-    );
-  });
-
-  it('should return an empty string for a valid password', () => {
-    const validPassword = 'ValidPassword1!';
-    const errorMessage = validatePassword(validPassword);
-    expect(errorMessage).toBe('');
+    invalidPasswords.forEach((password) => {
+      expect(validatePassword(password)).toBe(errorMessage);
+    });
   });
 });
