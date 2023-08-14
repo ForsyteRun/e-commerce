8b7230c5ee84d984331d7264e73786748695042e
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-14 17:03:09 +0300

    refactor: move sdk config vars to .env

--- src/services/sdkClient/apiRoot.ts
+++ src/services/sdkClient/apiRoot.ts
@@ -1,10 +1,9 @@
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 import ctpClient from './BuildClient';
-import { projectKey } from './constants';
 
 // Create apiRoot from the imported ClientBuilder and include your Project key
 const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
-  projectKey,
+  projectKey: import.meta.env.VITE_PROJECT_KEY,
 });
 
 export default apiRoot;
