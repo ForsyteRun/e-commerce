dca82e71f9bf227fcaaee92d4ccf175356dc83c8
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-17 13:13:43 +0300

    fix: remove unused anonymousClient at apiRoot that throws an error

--- src/services/sdkClient/apiRoot.ts
+++ src/services/sdkClient/apiRoot.ts
@@ -1,14 +1,8 @@
 import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
-import ctpClient, { anonymousClient } from './BuildClient';
+import ctpClient from './BuildClient';
 
 // Create apiRoot from the imported ClientBuilder and include your Project key
 const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
-  projectKey: import.meta.env.VITE_PROJECT_KEY,
-});
-
-export const anonymousApi = createApiBuilderFromCtpClient(
-  anonymousClient
-).withProjectKey({
   projectKey: import.meta.env.VITE_PROJECT_KEY,
 });
 
