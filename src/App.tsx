99c78e204bba0ceace664485c9e9cd42a3d62d1c
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-17 11:31:22 +0300

    refactor: remove test data and functions inside App

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,8 @@
 /* eslint-disable no-console */
-import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
-import useApi from './hooks/useApi';
-import useRefreshToken from './hooks/useRefreshToken';
-import { useAppDispatch } from './hooks/useRedux';
-import { setUserLoginState } from './store/userSlice';
 
 function App() {
-  const { anonymousApi, refreshTokenApi, logInUser } = useApi();
-  const { refreshToken, removeRefreshToken } = useRefreshToken();
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    const badTokenMessage =
-      'The refresh token was not found. It may have expired.';
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    logInUser({
-      email: 'mainuser1@gmail.com',
-      password: 'itspassworD1!',
-    });
-
-    if (refreshToken) {
-      console.log('Refresh Flow');
-      refreshTokenApi
-        .me()
-        .get()
-        .execute()
-        .then(() => {
-          console.log('Logged user');
-          dispatch(setUserLoginState(true));
-        })
-        .catch((e) => {
-          if (e.message === badTokenMessage) {
-            console.log('Refresh Token is out of date');
-            removeRefreshToken();
-            anonymousApi.me().carts().get().execute();
-          } else if (e.statusCode === 403) {
-            console.log('Anonymous user');
-            dispatch(setUserLoginState(false));
-          } else {
-            console.error(e);
-          }
-        });
-    } else {
-      console.log('Anonymous Flow');
-      anonymousApi.me().carts().get().execute();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
       <Header />
--- src/services/sdkClient/BuildClient.ts
+++ src/services/sdkClient/BuildClient.ts
@@ -1,18 +1,9 @@
 import { ClientBuilder } from '@commercetools/sdk-client-v2';
-import {
-  authMiddlewareOptions,
-  httpMiddlewareOptions,
-  anonymousAuthMiddlewareOptions,
-} from './constants';
+import { authMiddlewareOptions, httpMiddlewareOptions } from './constants';
 
 // Export the ClientBuilder
 const ctpClient = new ClientBuilder()
   .withClientCredentialsFlow(authMiddlewareOptions)
-  .withHttpMiddleware(httpMiddlewareOptions)
-  .build();
-
-export const anonymousClient = new ClientBuilder()
-  .withAnonymousSessionFlow(anonymousAuthMiddlewareOptions)
   .withHttpMiddleware(httpMiddlewareOptions)
   .build();
 
