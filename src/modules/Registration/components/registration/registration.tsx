164a8e43463f5bea891277a0fc647ec762d3d45e
ForsyteRun <forsyterun@gmail.com>
2023-08-17 08:47:42 +0300

    feat: add UI billing & shipping adress

--- src/modules/Registration/components/registration/registration.tsx
+++ src/modules/Registration/components/registration/registration.tsx
@@ -9,13 +9,13 @@ import React, { useState } from 'react';
 import renderSnackBar from '../../../../components/SnackBar/helpers';
 import apiRoot from '../../../../services/sdkClient/apiRoot';
 import { RequestStatusCode } from '../../../../types';
-import NavigateToLogin from '../NavigateToLogin';
-import Adress from '../adress/Adress';
+import NavigateToLogin from '../navigateToLogin';
 import validCountries from '../adress/constants';
 import Select from '../select/select';
 import { BIRTH_INIT_DATA } from './constant';
 import styles from './registration.module.scss';
 import { validateEmail, validateName, validatePassword } from './validation';
+import Adress from '../adress/Adress';
 
 const initialValues: CustomerDraft = {
   firstName: '',
@@ -53,69 +53,77 @@ const Registration: React.FC = () => {
 
   return (
     <div className={styles.register}>
+      <h1 className={styles.title}>REGISTRATION</h1>
       <Formik<CustomerDraft>
         initialValues={initialValues}
         onSubmit={(value: CustomerDraft) => createCustomer(value)}
       >
         {({ errors, touched }) => (
           <Form method="post" action="register" className={styles.form}>
-            <div className={styles.input__container}>
-              <label htmlFor="firstName" className={styles.label}>
-                FirstName
-              </label>
-              <Field
-                id="firstName"
-                name="firstName"
-                validate={validateName}
-                placeholder="First Name*"
-                className={styles.input}
-              />
-              {errors.firstName && touched.firstName && (
-                <div className={styles.errorValid}>{errors.firstName}</div>
-              )}
+            <div className={styles.name__container}>
+              <div className={styles.input__container}>
+                <label htmlFor="firstName" className={styles.label}>
+                  FirstName
+                </label>
+                <Field
+                  id="firstName"
+                  name="firstName"
+                  validate={validateName}
+                  placeholder="First Name*"
+                  className={styles.input}
+                />
+                {errors.firstName && touched.firstName && (
+                  <div className={styles.errorValid}>{errors.firstName}</div>
+                )}
+              </div>
+              <div className={styles.input__container}>
+                <label htmlFor="lastName">LastName</label>
+                <Field
+                  id="lastName"
+                  name="lastName"
+                  validate={validateName}
+                  placeholder="Last Name*"
+                  className={styles.input}
+                />
+                {errors.lastName && touched.lastName && (
+                  <div className={styles.errorValid}>{errors.lastName}</div>
+                )}
+              </div>
             </div>
-            <div className={styles.input__container}>
-              <label htmlFor="lastName">LastName</label>
-              <Field
-                id="lastName"
-                name="lastName"
-                validate={validateName}
-                placeholder="Last Name*"
-                className={styles.input}
-              />
-              {errors.lastName && touched.lastName && (
-                <div className={styles.errorValid}>{errors.lastName}</div>
-              )}
-            </div>
-            <div className={styles.input__container}>
-              <label htmlFor="email">Email</label>
-              <Field
-                id="email"
-                name="email"
-                validate={validateEmail}
-                placeholder="email*"
-                className={styles.input}
-              />
-              {errors.email && touched.email && (
-                <div className={styles.errorValid}>{errors.email}</div>
-              )}
-            </div>
-            <div className={styles.input__container}>
-              <label htmlFor="password">Password</label>
-              <Field
-                id="password"
-                type="password"
-                name="password"
-                validate={validatePassword}
-                placeholder="password*"
-                className={styles.input}
-              />
-              {errors.password && touched.password && (
-                <div className={styles.errorValid}>{errors.password}</div>
-              )}
+            <div className={styles.name__container}>
+              <div className={styles.input__container}>
+                <label htmlFor="email">Email</label>
+                <Field
+                  id="email"
+                  name="email"
+                  validate={validateEmail}
+                  placeholder="email*"
+                  className={styles.input}
+                />
+                {errors.email && touched.email && (
+                  <div className={styles.errorValid}>{errors.email}</div>
+                )}
+              </div>
+              <div className={styles.input__container}>
+                <label htmlFor="password">Password</label>
+                <Field
+                  id="password"
+                  type="password"
+                  name="password"
+                  validate={validatePassword}
+                  placeholder="password*"
+                  className={styles.input}
+                />
+                {errors.password && touched.password && (
+                  <div className={styles.errorValid}>{errors.password}</div>
+                )}
+              </div>
             </div>
             <Select />
-            <Adress />
+            <div className={styles.adress__container}>
+              <Adress blockTitle="Shipping adress" />
+              <Adress blockTitle="Billing adress" />
+            </div>
             <button type="submit">Register</button>
           </Form>
         )}
