e0a1ce6c149f890d7284ebe9e6766b3db7908b6c
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-18 23:43:15 +0300

    refactor: move extraReducers for userDataSlice to separate file

--- src/store/userDataSlice/index.ts
+++ src/store/userDataSlice/index.ts
@@ -1,12 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { _ErrorResponse } from '@commercetools/platform-sdk';
 import createAnonymousUser from './thunks/createAnonymousUser';
 import fetchUserDataByRefreshToken from './thunks/fetchUserDataByRefreshToken';
 import fetchUserLoginData from './thunks/fetchUserLoginData';
-import ERROR_MESSAGES from '../../pages/LoginPage/components/LoginForm/components/LoginError/constants';
-import setPendingStatus from './helpers/setPendingStatus';
-import setUserData from './helpers/setUserData';
-import { IUserState, RequestStatusCode } from '../../types';
+import extraReducers from './extraReducers';
+import { IUserState } from '../../types';
 
 const initialState: IUserState = {
   data: {
@@ -27,35 +24,7 @@ const userDataSlice = createSlice({
       state.error = null;
     },
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(createAnonymousUser.pending, setPendingStatus)
-      .addCase(createAnonymousUser.fulfilled, setUserData)
-      .addCase(createAnonymousUser.rejected, (state) => {
-        state.loading = 'failed';
-        state.error = 'Something went wrong...';
-      })
-      .addCase(fetchUserDataByRefreshToken.pending, setPendingStatus)
-      .addCase(fetchUserDataByRefreshToken.fulfilled, setUserData)
-      .addCase(fetchUserDataByRefreshToken.rejected, (state, { payload }) => {
-        state.loading = 'failed';
-        const error = payload as _ErrorResponse;
-        state.error = error.message;
-      })
-      .addCase(fetchUserLoginData.pending, setPendingStatus)
-      .addCase(fetchUserLoginData.fulfilled, setUserData)
-      .addCase(fetchUserLoginData.rejected, (state, { payload }) => {
-        state.loading = 'failed';
-        const error = payload as _ErrorResponse;
-        if (error.message === ERROR_MESSAGES.ACCOUNT_NOT_FOUND) {
-          state.error = error.message;
-        } else if (error.statusCode === RequestStatusCode.BadRequest) {
-          state.error = ERROR_MESSAGES.BAD_REQUEST;
-        } else {
-          state.error = ERROR_MESSAGES.GENERIC;
-        }
-      });
-  },
+  extraReducers,
 });
 
 export const { resetUserDataError } = userDataSlice.actions;
