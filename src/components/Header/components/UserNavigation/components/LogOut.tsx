51e047e537951b97ce413622594549f9b47a8ab2
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-18 12:25:26 +0300

    fix: remove redirection to main page on logout

--- src/components/Header/components/UserNavigation/components/LogOut.tsx
+++ src/components/Header/components/UserNavigation/components/LogOut.tsx
@@ -1,16 +1,12 @@
-import { useLocation, useNavigate } from 'react-router-dom';
 import useApi from '../../../../../hooks/useApi';
 import { useAppDispatch } from '../../../../../hooks/useRedux';
 import useRefreshToken from '../../../../../hooks/useRefreshToken';
 import { setUserLoginState } from '../../../../../store/userSlice';
-import { PathNames } from '../../../../../types';
 
 const LogOut = () => {
   const { anonymousApi } = useApi();
   const dispatch = useAppDispatch();
   const { removeRefreshToken } = useRefreshToken();
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
 
   const logoutHandler = () => {
     anonymousApi
@@ -21,9 +17,6 @@ const LogOut = () => {
       .then(() => {
         dispatch(setUserLoginState(false));
         removeRefreshToken();
-        if (pathname === PathNames.login || pathname === PathNames.register) {
-          navigate(PathNames.index);
-        }
       })
       // eslint-disable-next-line no-console
       .catch(console.error);
