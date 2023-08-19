5fb6ff94dd642e37a06f19fa43418b8ea59d64ab
Andrei Paleshka <andrepoleshko@gmail.com>
2023-08-19 23:52:25 +0200

    chore: configure aliases

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +10,14 @@ export default defineConfig({
   resolve: {
     alias: {
       'node-fetch': 'isomorphic-fetch',
+      assets: `${path.resolve(__dirname, './src/assets/')}`,
+      components: `${path.resolve(__dirname, './src/components/')}`,
+      hooks: `${path.resolve(__dirname, './src/hooks/')}`,
+      modules: `${path.resolve(__dirname, './src/modules/')}`,
+      pages: `${path.resolve(__dirname, './src/pages/')}`,
+      services: `${path.resolve(__dirname, './src/services/')}`,
+      store: `${path.resolve(__dirname, './src/store/')}`,
+      types: `${path.resolve(__dirname, './src/types/')}`,
     },
   },
   define: {
