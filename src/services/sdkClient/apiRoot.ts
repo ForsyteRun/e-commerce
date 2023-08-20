94cdf6acae76a47e9e90dc1711f59230b63bbe4c
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-20 10:44:13 +0300

    refactor: delete unused code

--- src/services/sdkClient/apiRoot.ts
+++
@@ -1,9 +0,0 @@
-import { createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
-import ctpClient from './BuildClient';
-
-// Create apiRoot from the imported ClientBuilder and include your Project key
-const apiRoot = createApiBuilderFromCtpClient(ctpClient).withProjectKey({
-  projectKey: import.meta.env.VITE_PROJECT_KEY,
-});
-
-export default apiRoot;
