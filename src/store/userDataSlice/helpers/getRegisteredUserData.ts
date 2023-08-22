ad520f41cdc57a5db0d2dd0e6bef12c1c7ebb192
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-22 21:10:33 +0300

    refactor: make getRegisteredUserData a little shorter

--- src/store/userDataSlice/helpers/getRegisteredUserData.ts
+++ src/store/userDataSlice/helpers/getRegisteredUserData.ts
@@ -1,34 +1,30 @@
 import { Customer } from '@commercetools/platform-sdk';
 import { RegisteredUserData } from 'types';
 
-const getRegisteredUserData = (data: Customer): RegisteredUserData => {
-  const {
-    id,
-    email,
-    firstName,
-    lastName,
-    dateOfBirth,
-    addresses,
-    defaultBillingAddressId,
-    defaultShippingAddressId,
-    shippingAddressIds,
-    billingAddressIds,
-    authenticationMode,
-  } = data;
-
-  return {
-    id,
-    email,
-    firstName,
-    lastName,
-    dateOfBirth,
-    addresses,
-    defaultBillingAddressId,
-    defaultShippingAddressId,
-    shippingAddressIds,
-    billingAddressIds,
-    authenticationMode,
-  };
-};
+const getRegisteredUserData = ({
+  id,
+  email,
+  firstName,
+  lastName,
+  dateOfBirth,
+  addresses,
+  defaultBillingAddressId,
+  defaultShippingAddressId,
+  shippingAddressIds,
+  billingAddressIds,
+  authenticationMode,
+}: Customer): RegisteredUserData => ({
+  id,
+  email,
+  firstName,
+  lastName,
+  dateOfBirth,
+  addresses,
+  defaultBillingAddressId,
+  defaultShippingAddressId,
+  shippingAddressIds,
+  billingAddressIds,
+  authenticationMode,
+});
 
 export default getRegisteredUserData;
