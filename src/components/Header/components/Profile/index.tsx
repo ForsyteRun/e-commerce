88e746bfd74da6400efe0ce8ecabc1c12b6cb9a5
ForsyteRun <forsyterun@gmail.com>
2023-08-23 13:03:23 +0300

    feat: add profileIcon

---
+++ src/components/Header/components/Profile/index.tsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const Profile = () => {
+  return (
+    <Box sx={{ cursor: 'pointer' }}>
+      <AccountCircleIcon fontSize="large" />
+    </Box>
+  );
+};
+
+export default Profile;
