159b4b34373a909360f9429114d452a60fe3b2c1
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-24 12:26:31 +0300

    refactor: add test for isCorrectAttributeType

--- src/store/helpers/transformProductData/isCorrectAttributeType.ts
+++ src/store/helpers/transformProductData/isCorrectAttributeType.ts
@@ -1,7 +1,7 @@
 import { AttributeValue } from 'types';
 
 const isCorrectAttributeType = (attr: unknown): attr is AttributeValue => {
-  const isNumber = typeof attr === 'number';
+  const isNumber = typeof attr === 'number' && !Number.isNaN(attr);
   const isBoolean = typeof attr === 'boolean';
   const isString = typeof attr === 'string';
   return isNumber || isBoolean || isString;
