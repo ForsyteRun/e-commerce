2b6537dfc86a47d106fdb03ea87ee3dd3a8ecba8
Andrei Paleshka <andrepoleshko@gmail.com>
2023-08-24 13:20:18 +0200

    feat: add nav link button for catalog page and additional wrappers

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import NavLinkButton from 'UI/NavLinkButton';
+import { PathNames } from 'types';
 import UserNavigation from './components/UserNavigation';
 import Logo from './components/Logo';
 import styles from './Header.module.scss';
@@ -6,9 +8,16 @@ import Search from './components/Search';
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Logo />
-      <Search />
-      <UserNavigation />
+      <div className={styles.headerContent}>
+        <Logo />
+        <div className={styles.container}>
+          <NavLinkButton path={PathNames.catalog}>Catalog</NavLinkButton>
+          <div className={styles.headerElements}>
+            <Search />
+            <UserNavigation />
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
