3e68f580e9039d9ffdd1743e1fd64cde3d613595
ForsyteRun <forsyterun@gmail.com>
2023-08-27 20:45:57 +0300

    feat: add style aside

--- src/modules/UserProfile/components/AsideMenu/index.tsx
+++ src/modules/UserProfile/components/AsideMenu/index.tsx
@@ -15,17 +15,20 @@ const AsideMenu = () => {
       <Stack
         sx={{
           flexDirection: 'row',
-          alignItems: 'center',
           justifyContent: 'space-between',
+          alignItems: 'center',
           gap: '20px',
         }}
       >
         <div className={styles.title}>Welcome, {firstName || 'User'}</div>
         <LogOutButton />
       </Stack>
-      <Stack>
+      <Stack className={styles.links__block}>
         <NavLink to={PathNames.profileInfo}>Personal information</NavLink>
         <NavLink to={PathNames.profileAddress}>Address book</NavLink>
+        <NavLink to={PathNames.profileAddress} className={styles.disable}>
+          Change password
+        </NavLink>
       </Stack>
     </aside>
   );
