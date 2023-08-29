16a9db3546635073846371fa3b32716d5ead7fd0
ForsyteRun <forsyterun@gmail.com>
2023-08-29 10:56:16 +0300

    feat: render billing&shipping fields

--- src/modules/UserProfile/components/Content/AddressBook/index.tsx
+++ src/modules/UserProfile/components/Content/AddressBook/index.tsx
@@ -1,17 +1,17 @@
+import { BaseAddress } from '@commercetools/platform-sdk';
 import { Typography, Box, Stack } from '@mui/material';
 import { useAppSelector } from 'hooks/useRedux';
 import { RegisteredUserData } from 'types';
+import AddressBlock from './components/AddressBlock';
+import { AddressTypesEnum } from './types';
 
 const AddressBook = () => {
   const { addresses } = useAppSelector(
     (state) => state.userDataSlice.data
   ) as RegisteredUserData;
 
-  console.log(addresses);
-
   return (
     <>
-      street, city, state, zip code, country
       <Box sx={{ mb: '4rem' }}>
         <Typography variant="h3" sx={{ mb: '1rem' }}>
           Address book
@@ -21,25 +21,19 @@ const AddressBook = () => {
           address
         </Typography>
       </Box>
+
       <Stack sx={{ flexDirection: 'row', justifyContent: 'space-around' }}>
-        <Stack>
-          <Typography variant="h6" sx={{ mb: '1rem' }}>
-            Billing address
-          </Typography>
-          <Stack>
-            <Typography variant="h6" sx={{ mb: '1rem' }}>
-              street {}
-            </Typography>
-            <Typography variant="h6" sx={{ mb: '1rem' }}>
-              Billing address
-            </Typography>
-          </Stack>
-        </Stack>
-        <Stack>
-          <Typography variant="h6" sx={{ mb: '1rem' }}>
-            Shipping address
-          </Typography>
-        </Stack>
+        {addresses &&
+          addresses.map((address: BaseAddress, index) => (
+            <Stack key={address.id}>
+              <AddressBlock
+                title={
+                  index ? AddressTypesEnum.billing : AddressTypesEnum.shipping
+                }
+                address={address}
+              />
+            </Stack>
+          ))}
       </Stack>
     </>
   );
---
+++ src/modules/UserProfile/components/Content/AddressBook/types.ts
@@ -0,0 +1,6 @@
+export enum AddressTypesEnum {
+  billing = 'Billing address',
+  shipping = 'Shipping address',
+}
+
+export default AddressTypesEnum;
