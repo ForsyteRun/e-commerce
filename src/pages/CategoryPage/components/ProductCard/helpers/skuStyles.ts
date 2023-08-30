7bc89108027dc24319096addffc7febdb435edf7
Andrei Paleshka <andrepoleshko@gmail.com>
2023-08-30 23:34:39 +0200

    refactor: create separate styling files for product cards

---
+++ src/pages/CategoryPage/components/ProductCard/helpers/skuStyles.ts
@@ -0,0 +1,12 @@
+import { SxProps } from '@mui/material';
+
+const skuStyles: SxProps = {
+  backgroundColor: 'rgba(103, 58, 183, 0.2)',
+  borderRadius: '5px',
+  borderLeft: '5px solid #673ab7',
+  color: '#000000',
+  fontSize: '12px',
+  maxWidth: 'max-content',
+  padding: '3px',
+};
+export default skuStyles;
