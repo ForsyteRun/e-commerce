f325e99ce6d5bfe3e1c1528beb63204d8d804b2a
ForsyteRun <forsyterun@gmail.com>
2023-08-31 15:28:35 +0300

    feat: add edit mode UI

--- src/modules/UserProfile/components/Content/UserInfo/components/FieldInfo/index.tsx
+++ src/modules/UserProfile/components/Content/UserInfo/components/FieldInfo/index.tsx
@@ -1,24 +1,49 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, Typography, InputAdornment, Button } from '@mui/material';
 import React from 'react';
 import ModeIcon from '@mui/icons-material/Mode';
+import TextField from '@mui/material/TextField';
 import styles from './fieldInfo.module.scss';
 
 interface IFieldInfo {
   value: string | undefined;
   title: string;
 }
-
 const FieldInfo: React.FC<IFieldInfo> = ({ value, title }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <Stack
-      sx={{ flexDirection: 'row', justifyContent: 'space-between' }}
+      sx={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}
       className={styles.info}
     >
-      <Typography variant="h5">{title}</Typography>
-      <Typography variant="h5" sx={{ flexBasis: '50%' }}>
-        {value}
-      </Typography>
-      <ModeIcon sx={{ cursor: 'pointer' }} />
+      {!open ? (
+        <>
+          <Typography variant="h5">{title}</Typography>
+          <Typography variant="h5" sx={{ flexBasis: '50%' }}>
+            {value}
+          </Typography>
+          <ModeIcon sx={{ cursor: 'pointer' }} onClick={handleClick} />
+        </>
+      ) : (
+        <TextField
+          placeholder={`enter ${title}`}
+          sx={{ width: '100%' }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Button>submit</Button>
+              </InputAdornment>
+            ),
+          }}
+        />
+      )}
     </Stack>
   );
 };
