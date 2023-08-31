0754980ff7004fdf6220d6f2164193c0f7e110c2
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-31 15:06:19 +0300

    feat: implement navigation to product page

--- src/pages/CategoryPage/components/ProductCard/index.tsx
+++ src/pages/CategoryPage/components/ProductCard/index.tsx
@@ -1,6 +1,7 @@
 import { Card } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-import { IProductData, PathNames } from 'types';
+import { generateProductPath } from 'helpers/pathGenerators';
+import { IProductData } from 'types';
 import cardStyles from './cardStyle';
 import {
   ProductImage,
@@ -9,11 +10,13 @@ import {
 } from './components';
 
 const ProductCard = ({ data }: { data: IProductData }) => {
-  const { name, images, price, attributes, sku } = data;
+  const { name, images, price, attributes, sku, slug, categories } = data;
   const image = images ? images[0] : undefined;
 
+  const path = generateProductPath(slug, categories[0]);
+
   return (
-    <Card component={NavLink} to={PathNames.index} sx={cardStyles}>
+    <Card component={NavLink} to={path} sx={cardStyles}>
       <ProductImage name={name} url={image} />
       <ProductCardContent attributes={attributes} name={name} sku={sku} />
       {price && <ProductCardPrice price={price} />}
