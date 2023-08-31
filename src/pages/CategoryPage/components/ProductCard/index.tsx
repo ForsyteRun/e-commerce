345fc6c90a621f74101c4da8b55908762744d55a
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-31 20:48:29 +0300

    Navigation to product page (#62)
    * refactor: split product card component on smaller components
    
    * refactor: finish split of product card component
    
    * fix: fix price view on product cards
    
    * feat: implement navigation to product page

--- src/pages/CategoryPage/components/ProductCard/index.tsx
+++ src/pages/CategoryPage/components/ProductCard/index.tsx
@@ -1,83 +1,25 @@
-/* eslint-disable react/prop-types */
-import DiscountIcon from '@mui/icons-material/Discount';
-import {
-  Button,
-  Card,
-  CardContent,
-  CardActions,
-  Typography,
-} from '@mui/material';
+import { Card } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-import { IProductData, PathNames } from 'types';
-import buttonStyles from './helpers/buttonStyles';
-import cardStyles from './helpers/cardStyle';
-import formatPrintAttributes from './helpers/formatPrintAttributes';
-import skuStyles from './helpers/skuStyles';
-import styles from './ProductCard.module.scss';
+import { generateProductPath } from 'helpers/pathGenerators';
+import { IProductData } from 'types';
+import cardStyles from './cardStyle';
+import {
+  ProductImage,
+  ProductCardContent,
+  ProductCardPrice,
+} from './components';
 
-const ProductCard: React.FC<{
-  data: IProductData;
-}> = ({ data }) => {
-  const { name, images, price, attributes, sku } = data;
+const ProductCard = ({ data }: { data: IProductData }) => {
+  const { name, images, price, attributes, sku, slug, categories } = data;
+  const image = images ? images[0] : undefined;
+
+  const path = generateProductPath(slug, categories[0]);
 
   return (
-    <Card sx={cardStyles} component={NavLink} to={PathNames.index}>
-      <div className={styles.productImage}>
-        {images && (
-          <img src={images[0]} alt="printer" className={styles.image} />
-        )}
-      </div>
-      <CardContent className={styles.cardInfo}>
-        <Typography
-          variant="h5"
-          className={styles.title}
-          sx={{ fontSize: '16px', fontWeight: '700', margin: '10px 0' }}
-        >
-          {name}
-        </Typography>
-        <Typography className={styles.sku} sx={skuStyles}>
-          {`SKU: ${sku}`}
-        </Typography>
-        {attributes && (
-          <div className={styles.attributes}>
-            {formatPrintAttributes(attributes)}
-          </div>
-        )}
-      </CardContent>
-      <CardActions className={styles.cardActions}>
-        {price && (
-          <div className={styles.priceInfo}>
-            <Typography
-              className={styles.value}
-              sx={{
-                fontWeight: '700',
-                fontSize: '18px',
-                textDecoration: price.discounted ? 'line-through' : 'none',
-              }}
-            >
-              {`€${price.net}`}
-            </Typography>
-            {price.discounted && (
-              <Typography
-                className={styles.discount}
-                sx={{
-                  fontWeight: '700',
-                  fontSize: '16px',
-                }}
-              >
-                <DiscountIcon
-                  fontSize="small"
-                  className={styles.discountIcon}
-                />
-                {`€${price.discounted}`}
-              </Typography>
-            )}
-          </div>
-        )}
-        <Button variant="outlined" sx={buttonStyles}>
-          Add to cart
-        </Button>
-      </CardActions>
+    <Card component={NavLink} to={path} sx={cardStyles}>
+      <ProductImage name={name} url={image} />
+      <ProductCardContent attributes={attributes} name={name} sku={sku} />
+      {price && <ProductCardPrice price={price} />}
     </Card>
   );
 };
