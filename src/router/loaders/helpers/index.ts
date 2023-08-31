bf9785bd916cd238498461d9096d67790e203e3a
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-08-31 21:42:15 +0300

    refactor: delete usesless function for fetch single category

--- src/router/loaders/helpers/index.ts
+++ src/router/loaders/helpers/index.ts
@@ -1,13 +1,6 @@
-import fetchCategoryWith from './fetchCategoryWith';
 import throwRouteError from './throwRouteError';
 import mapSplatArray from './mapSplatArray';
 import getParentId from './getParentId';
 import checkProductExists from './checkProductExists';
 
-export {
-  fetchCategoryWith,
-  throwRouteError,
-  mapSplatArray,
-  getParentId,
-  checkProductExists,
-};
+export { throwRouteError, mapSplatArray, getParentId, checkProductExists };
