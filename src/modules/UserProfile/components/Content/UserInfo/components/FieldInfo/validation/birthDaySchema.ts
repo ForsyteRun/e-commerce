c83237a1645ee419e08c3b31fe227a4ba2d3a769
ForsyteRun <forsyterun@gmail.com>
2023-09-02 09:19:34 +0300

    fix: correct validation dateOfBirth

--- src/modules/UserProfile/components/Content/UserInfo/components/FieldInfo/validation/birthDaySchema.ts
+++ src/modules/UserProfile/components/Content/UserInfo/components/FieldInfo/validation/birthDaySchema.ts
@@ -1,11 +1,29 @@
+/* eslint-disable no-restricted-globals */
 import * as Yup from 'yup';
 
 const dateOfBirthSchema = Yup.object().shape({
   dateOfBirth: Yup.string()
-    .matches(
-      /^(19|20)\d\d-(0[1-9]|1[0-2])-([0-2][1-9]|3[01])$/,
-      'Date must be in the format YYYY-MM-DD'
-    )
+    .test('valid-date', 'Invalid date format (YYYY-MM-DD)', (value) => {
+      if (!value) return true;
+      const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+      if (!dateRegex.test(value)) return false;
+      return true;
+    })
+    .test('valid-month', 'Incorrect month', (value) => {
+      if (!value) return true;
+      const [, month] = value.split('-').map(Number);
+      return !isNaN(month) && month >= 1 && month <= 12;
+    })
+    .test('valid-day', 'Incorrect day', (value) => {
+      if (!value) return true;
+      const [, , day] = value.split('-').map(Number);
+      return !isNaN(day) && day >= 1 && day <= 31;
+    })
+    .test('valid-year', 'Year must be after 1950', (value) => {
+      if (!value) return true;
+      const [year] = value.split('-').map(Number);
+      return year >= 1950 && year <= new Date().getFullYear();
+    })
     .test('minimumAge', 'Must be min 13 years old', (value) => {
       if (!value) return false; // Required field
       const minAgeDate = new Date();
