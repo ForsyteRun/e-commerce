db68638bfb38ba16ab8f7469cdee6964935d63c5
ForsyteRun <forsyterun@gmail.com>
2023-09-02 11:32:55 +0300

    refactor: replace API update data to store

---
+++ src/modules/UserProfile/api/updateUserData.ts
@@ -0,0 +1,26 @@
+// import { MyCustomerUpdate, _ErrorResponse } from '@commercetools/platform-sdk';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import createRefreshTokenClientApi from 'services/sdkClient/createRefreshTokenClientApi';
+// import { getRegistrationAccessCode } from 'store/registration/registrationAccess.slice';
+// import { getRegisteredUserData } from 'store/userDataSlice/helpers';
+
+// const updateUserData = createAsyncThunk(
+//   'userData/update',
+//   async (_body: MyCustomerUpdate, { rejectWithValue, dispatch }) => {
+//     const api = createRefreshTokenClientApi();
+
+//     const response = await api
+//       .me()
+//       .post({ body: _body })
+//       .execute()
+//       .then((res) => {
+//         dispatch(getRegistrationAccessCode(res.statusCode as number));
+//         return getRegisteredUserData(res.body);
+//       })
+//       .catch((err: _ErrorResponse) => rejectWithValue({ ...err }));
+
+//     return response;
+//   }
+// );
+
+// export default updateUserData;
