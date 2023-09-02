959a4076c128a36562fee7d6945dc93ba60c521b
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-09-02 14:08:05 +0300

    feat: implement breadcrumbs correct visualization and routings

--- src/pages/CatalogPage/components/Breadcrumbs/index.tsx
+++ src/pages/CatalogPage/components/Breadcrumbs/index.tsx
@@ -1,58 +1,28 @@
-/* eslint-disable react/no-array-index-key */
+import { useState, useEffect } from 'react';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import { Breadcrumbs, Typography } from '@mui/material';
-import { NavLink, useLocation } from 'react-router-dom';
-import { PathNames } from 'types';
-import styles from './Breadcrumbs.module.scss';
+import { Breadcrumbs } from '@mui/material';
+import { useLocation } from 'react-router-dom';
+import { mapSlugs, mapCrumbs } from './helpers';
+import { ISlugParam } from './types';
 
 const BreadcrumbsNavigation = () => {
-  const location = useLocation();
-  const pathnames = location.pathname.split('/').filter((x) => x);
+  const { pathname } = useLocation();
+  const [params, setParams] = useState<ISlugParam[]>([]);
 
-  const breadcrumbNameMap: { [key: string]: string } = {
-    '/catalog': 'Catalog',
-    '/catalog/printers': 'Printers',
-    '/catalog/mfp': 'MFP',
-    '/catalog/laser-printers': 'Laser Printers',
-    '/catalog/inkjet-printers': 'Inkjet Printers',
-    '/catalog/laser-mfps': 'Laser MFPs',
-    '/catalog/inkjet-mfps': 'Inkjet MFPs',
-  };
+  useEffect(() => {
+    const formattedPathname = pathname.replace(/\/*$/, '');
+    const slugs = formattedPathname.split('/');
+    const mappedSlugs = mapSlugs(slugs);
+    setParams(mappedSlugs);
+  }, [pathname]);
 
   return (
-    <div className={styles.container}>
-      <Breadcrumbs
-        separator={<NavigateNextIcon fontSize="small" />}
-        aria-label="breadcrumb"
-      >
-        <NavLink
-          color="inherit"
-          to={PathNames.index}
-          className={styles.breadcrumbsLink}
-        >
-          Home
-        </NavLink>
-        {pathnames.map((_, index) => {
-          const last: boolean = index === pathnames.length - 1;
-          const to = `/${pathnames.slice(0, index + 1).join('/')}`;
-
-          return last ? (
-            <Typography key={index} sx={{ fontSize: '14px' }}>
-              {breadcrumbNameMap[to]}
-            </Typography>
-          ) : (
-            <NavLink
-              key={index}
-              color="inherit"
-              to={to}
-              className={styles.breadcrumbsLink}
-            >
-              {breadcrumbNameMap[to]}
-            </NavLink>
-          );
-        })}
-      </Breadcrumbs>
-    </div>
+    <Breadcrumbs
+      separator={<NavigateNextIcon fontSize="small" />}
+      aria-label="breadcrumb"
+    >
+      {mapCrumbs(params)}
+    </Breadcrumbs>
   );
 };
 
