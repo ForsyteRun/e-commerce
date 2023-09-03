b8b5fb88683d9d921a0d859ecc40d0e181b4546e
ForsyteRun <forsyterun@gmail.com>
2023-09-03 06:31:29 +0300

    feat: separate Password

--- src/store/userDataSlice/thunks/updatePassword.ts
+++ src/store/userDataSlice/thunks/updatePassword.ts
@@ -5,19 +5,38 @@ import {
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import createRefreshTokenClientApi from 'services/sdkClient/createRefreshTokenClientApi';
 import { getRegistrationAccessCode } from 'store/registration/registrationAccess.slice';
-import { RequestStatusCode } from 'types';
+import { LoginFormValues, RequestStatusCode } from 'types';
+import fetchUserLoginData from './fetchUserLoginData';
 
 const updatePassword = createAsyncThunk(
   'userData/updatePassword',
   async (data: CustomerChangePassword, { dispatch }) => {
     const api = createRefreshTokenClientApi();
+    console.log(api);
 
     const response = await api
       .customers()
       .password()
       .post({ body: data })
       .execute()
-      .then(() => dispatch(getRegistrationAccessCode(RequestStatusCode.OK)))
+      .then((res) => {
+        const isPasswordUpdate =
+          res.statusCode && res.statusCode === RequestStatusCode.OK;
+        console.log('out');
+
+        if (isPasswordUpdate) {
+          console.log('in');
+
+          dispatch(getRegistrationAccessCode(res.statusCode!));
+
+          const loginData: LoginFormValues = {
+            email: res.body.email,
+            password: res.body.password!,
+          };
+
+          dispatch(fetchUserLoginData(loginData));
+        }
+      })
       .catch((err: _ErrorResponse) =>
         dispatch(getRegistrationAccessCode(err.statusCode))
       );
