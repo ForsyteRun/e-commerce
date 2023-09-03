1ebed012aae4b4ee132a9d37ede54a10f367f41d
Dmytro Markuntovych <sunsn4ke@gmail.com>
2023-09-03 19:51:38 +0300

    Sorting by price and name (#74)
    * feat: implement sorting by price and name
    
    * fix: fix product card with for smaller displzy sizes
    
    * refactor: control panel styles

--- src/pages/CategoryPage/components/Sort/index.tsx
+++ src/pages/CategoryPage/components/Sort/index.tsx
@@ -1,29 +1,11 @@
-/* eslint-disable react/prop-types */
-import { Select, MenuItem } from '@mui/material';
+import SortButton from './components/SortButton';
 import styles from './Sort.module.scss';
 
-const Sort: React.FC<{
-  sortOption: string;
-  setSortOption: (option: string) => void;
-}> = ({ sortOption, setSortOption }) => {
+const Sort = () => {
   return (
     <div className={styles.container}>
-      <Select
-        value={sortOption}
-        onChange={(e) => setSortOption(e.target.value)}
-        displayEmpty
-        sx={{
-          backgroundColor: 'white',
-          borderRadius: '10px',
-          '& fieldset': {
-            border: '1px solid #eaebed',
-          },
-        }}
-      >
-        <MenuItem value="default">Default</MenuItem>
-        <MenuItem value="priceAsc">Sort By: Price - Low to High</MenuItem>
-        <MenuItem value="priceDesc">Sort By: Price - High to Low</MenuItem>
-      </Select>
+      <SortButton by="price" />
+      <SortButton by="name" />
     </div>
   );
 };
--- src/router/loaders/getCategoryData.ts
+++ src/router/loaders/getCategoryData.ts
@@ -2,6 +2,7 @@ import { LoaderFunction } from 'react-router-dom';
 import fetchCategoriesList from 'store/categoriesSlice/fetchCategoriesList';
 import fetchProductsData from 'store/productsDataSlice/fetchProductsData';
 import store from 'store';
+import { resetSort } from 'store/sortSlice';
 import { throwRouteError } from './helpers';
 
 const getCategoryData: LoaderFunction = async ({ params }) => {
@@ -30,6 +31,8 @@ const getCategoryData: LoaderFunction = async ({ params }) => {
   } else {
     dispatch(fetchProductsData());
   }
+
+  dispatch(resetSort());
 
   return { ok: true };
 };
