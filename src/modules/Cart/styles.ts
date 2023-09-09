451868d7861161e132f25a4e0369c1a87d0cfaf6
ForsyteRun <forsyterun@gmail.com>
2023-09-09 08:39:55 +0300

    feat: add adaptive

--- src/modules/Cart/styles.ts
+++ src/modules/Cart/styles.ts
@@ -4,9 +4,16 @@ export const container: SxProps = {
   flexDirection: 'row',
   justifyContent: 'space-around',
   width: '100%',
+  '@media (max-width: 580px)': {
+    flexDirection: 'column',
+    gap: '1rem',
+  },
 };
 
 export const mainBlock: SxProps = {
   p: '1rem 1.5rem',
   flexBasis: '50%',
+  '@media (max-width: 768px)': {
+    flexBasis: '57%',
+  },
 };
