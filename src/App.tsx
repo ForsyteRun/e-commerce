d69bd14c0abc68179a416603495e366136cda01b
ForsyteRun <forsyterun@gmail.com>
2023-09-09 09:27:09 +0300

    refactor: normalize -> scc baseline

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Header from 'components/Header';
 import BurgerModal from 'components/BurgerModal';
 import { AppProvider } from 'context';
 import { identificateUserOnAppStart } from 'helpers';
+import CssBaseline from '@mui/material/CssBaseline';
 import styles from './App.module.scss';
 
 const App = () => {
@@ -14,15 +15,18 @@ const App = () => {
 
   return (
     <AppProvider>
-      <div className={styles.container}>
-        <BurgerModal />
-        <Header />
-        <main>
-          <Wrapper>
-            <Outlet />
-          </Wrapper>
-        </main>
-      </div>
+      <>
+        <CssBaseline />
+        <div className={styles.container}>
+          <BurgerModal />
+          <Header />
+          <main>
+            <Wrapper>
+              <Outlet />
+            </Wrapper>
+          </main>
+        </div>
+      </>
     </AppProvider>
   );
 };
