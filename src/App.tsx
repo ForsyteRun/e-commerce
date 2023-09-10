b225c67361e3fa6834652d95268915940ad3e40e
ForsyteRun <forsyterun@gmail.com>
2023-09-10 11:53:44 +0300

    feat: add MUI theme & customize SCCBaseLine

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import Wrapper from 'UI/Wrapper';
-import Header from 'components/Header';
 import BurgerModal from 'components/BurgerModal';
+import Header from 'components/Header';
 import { AppProvider } from 'context';
 import { identificateUserOnAppStart } from 'helpers';
-import CssBaseline from '@mui/material/CssBaseline';
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import theme from './theme';
 import styles from './App.module.scss';
 
 const App = () => {
@@ -15,7 +17,7 @@ const App = () => {
 
   return (
     <AppProvider>
-      <>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className={styles.container}>
           <BurgerModal />
@@ -26,7 +28,7 @@ const App = () => {
             </Wrapper>
           </main>
         </div>
-      </>
+      </ThemeProvider>
     </AppProvider>
   );
 };
