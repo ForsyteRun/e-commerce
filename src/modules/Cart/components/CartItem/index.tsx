13abd75d3e97c30b1de46be700b718fc2335b4d0
ForsyteRun <forsyterun@gmail.com>
2023-09-10 12:01:24 +0300

    fix: correct styles img

--- src/modules/Cart/components/CartItem/index.tsx
+++ src/modules/Cart/components/CartItem/index.tsx
@@ -11,13 +11,13 @@ const CartItem = ({ item }: ICartItem) => {
   const { name, quantity, variant, price } = item;
   return (
     <Stack flexDirection="row" gap="1rem" sx={{ p: '1rem 0.5rem' }}>
-      <Box>
+      <Box sx={{ width: '70px', height: '70px' }}>
         <img
           src={variant.images && variant.images[0].url}
           alt=""
           srcSet=""
-          width={70}
-          height={70}
+          width="100%"
+          height="100%"
         />
       </Box>
       <Stack sx={{ flex: 1, ml: '1rem' }} justifyContent="space-between">
