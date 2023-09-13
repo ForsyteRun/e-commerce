4b3faee4b82fa95b69181bc5e80ff2d14cfde4e2
ForsyteRun <forsyterun@gmail.com>
2023-09-13 08:32:11 +0300

    feat: add discount field Formik

--- src/modules/Cart/components/DiscountField/index.tsx
+++ src/modules/Cart/components/DiscountField/index.tsx
@@ -1,20 +1,41 @@
+import { MyCartUpdateAction } from '@commercetools/platform-sdk';
 import { Button } from '@mui/material';
 import { Field, Form, Formik } from 'formik';
+import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
+import { updateCart } from 'store/cartSlice/thunks';
 
 const DiscountField = () => {
+  const dispatch = useAppDispatch();
+  const { error } = useAppSelector((state) => state.cartSlice);
+
   return (
     <Formik
       initialValues={{ code: '' }}
-      onSubmit={(value) => console.log(value)}
+      onSubmit={(value: { code: string }) => {
+        const action: MyCartUpdateAction = {
+          action: 'addDiscountCode',
+          code: value.code,
+        };
+
+        dispatch(updateCart(action));
+      }}
     >
-      <Form>
+      <Form
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: '1rem',
+        }}
+      >
         <Field
           name="code"
           label="Enter promo code"
           variant="outlined"
-          fullWidth
           placeholder="Enter promo code"
+          style={{ padding: '9px', marginRight: '1rem' }}
         />
+        {error && <div>Not found</div>}
         <Button variant="contained" color="primary" type="submit">
           Use
         </Button>
