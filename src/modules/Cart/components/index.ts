89fb63f54ad7de09802b12ee7f7e9012f2b606d7
ForsyteRun <forsyterun@gmail.com>
2023-09-13 07:41:38 +0300

    feat: add input for promoCodes

--- src/modules/Cart/components/index.ts
+++ src/modules/Cart/components/index.ts
@@ -1,5 +1,6 @@
 import EmptyCart from './EmptyCart';
 import PriceBlock from './PriceBlock';
 import CartItem from './CartItem';
+import DiscountField from './DiscountField';
 
-export { EmptyCart, PriceBlock, CartItem };
+export { EmptyCart, PriceBlock, CartItem, DiscountField };
