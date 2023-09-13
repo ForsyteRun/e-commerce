1be8fe4b25dd9fda007df6a5a90e263d0c0019be
ForsyteRun <forsyterun@gmail.com>
2023-09-13 08:32:11 +0300

    feat: add discount field Formik

--- src/pages/CartPage/index.tsx
+++ src/pages/CartPage/index.tsx
@@ -1,25 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { MyCartUpdateAction } from '@commercetools/platform-sdk';
-import { useAppDispatch, useAppSelector } from 'hooks/useRedux';
+import { useAppSelector } from 'hooks/useRedux';
 import Cart from 'modules/Cart';
 import { EmptyCart } from 'modules/Cart/components';
-import { useEffect } from 'react';
-import { updateCart } from 'store/cartSlice/thunks';
 
 const CartPage = () => {
   const { data } = useAppSelector((state) => state.cartSlice);
-  const dispatch = useAppDispatch();
 
   const isCartNotEmpty = !!data?.lineItems.length;
-
-  useEffect(() => {
-    const action: MyCartUpdateAction = {
-      action: 'addDiscountCode',
-      code: 'summer',
-    };
-
-    dispatch(updateCart(action));
-  }, []);
 
   return isCartNotEmpty ? <Cart lineItems={data.lineItems} /> : <EmptyCart />;
 };
