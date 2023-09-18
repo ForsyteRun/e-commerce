264c0383c2bddc24f8548dfdd0157cdf050a79eb
Andrei Paleshka <andrepoleshko@gmail.com>
2023-09-18 23:35:46 +0200

    fix: fix the return value of the MainPage component

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -2,9 +2,13 @@ import PromoCodeCard from './components/PromoCodeCard';
 import promoCodesData from './constants';
 
 const MainPage = () => {
-  return promoCodesData.map((data) => (
-    <PromoCodeCard key={data.code} data={data} />
-  ));
+  return (
+    <>
+      {promoCodesData.map((data) => (
+        <PromoCodeCard key={data.code} data={data} />
+      ))}
+    </>
+  );
 };
 
 export default MainPage;
